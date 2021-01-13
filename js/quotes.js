0b67fbf7ca9cafb62fd160bb19be7325fad7795a
nicoya7791 <hblandon750@outlook.com>
2021-01-12 19:49:18 -0800

    extra credit one completed

--- js/quotes.js
+++ js/quotes.js
@@ -1,43 +1,48 @@
 const quotes = [
-    {
-      quote: "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.", 
-      source: "Patrick McKenzie", 
-      citation: "Twitter",
-      year: 2016,
-      profession: "Computer Science"
-      
-    },
-    {
-      quote: "Nothing is certain except for death and taxes.",
-      source: "Christopher Bullock",
-      citation: "The Cobler of Preston",
-      year: 1716,
-      profession: "Musician"
-    },
-    {
-      quote: "The greatest wealth is to live content with little.",
-      source: "Plato",
-      profession: "Philospher"
-    },
-    {
-      quote: "Never discourage anyone who continually makes progress, no matter how slow.",
-      source: "Plato", 
-      profession: "Philospher"
-      },
-    {
-      quote: "Every accomplishment starts with the decision to try",
-      source: "John F. Kennedy",
-      profession: "Politician"
-      },
-    {
-      quote: "Every accomplishment starts with the decision to try",
-      source: "Martin Luther King Jr. ",
-      citation: "Letter from Birmingham jail",
-      year: 1963,
-      profession: "Civil Right Activist"
-    }
-    
-  ]
- 
-
-  
+	{
+		quote:
+			"Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.",
+		source: "Patrick McKenzie",
+		citation: "Twitter",
+		year: 2016,
+		profession: "Computer Science",
+	},
+	{
+		quote: "Nothing is certain except for death and taxes.",
+		source: "Christopher Bullock",
+		citation: "The Cobler of Preston",
+		year: 1716,
+		profession: "Musician",
+	},
+	{
+		quote: "The greatest wealth is to live content with little.",
+		source: "Plato",
+		profession: "Philosopher",
+	},
+	{
+		quote:
+			"Never discourage anyone who continually makes progress, no matter how slow.",
+		source: "Plato",
+		profession: "Philosopher",
+	},
+	{
+		quote: "Every accomplishment starts with the decision to try.",
+		source: "John F. Kennedy",
+		profession: "Politician",
+	},
+	{
+		quote:
+			"Oppressed people cannot remain oppressed forever. The yearning for freedom eventually manifests itself.",
+		source: "Martin Luther King Jr. ",
+		citation: "Letter from Birmingham jail",
+		year: 1963,
+		profession: "Civil Rights Activist",
+	},
+	{
+		quote:
+			"Float like a butterfly, sting like a bee. The hands can't hit what the eyes can't see.",
+		source: "Muhammad Ali ",
+		year: 1964,
+		profession: "Proffesional Boxer",
+	},
+];
--- js/script.js
+++ js/script.js
@@ -3,53 +3,54 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// For assistance:
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+/***
+ * `quotes` array
+ ***/
 //===>array is in quotes.js
 // let html = document.querySelector(".object");
 
 /***
  * `getRandomQuote` function
-***/
+ ***/
 //
-function getRandomQuote(){
-  let a= quotes.length-1;
-  let randonNumber = Math.floor(Math.random()*a +1);
-  return quotes[randonNumber];
+function getRandomQuote() {
+	let a = quotes.length - 1;
+	let randonNumber = Math.floor(Math.random() * a + 1);
+	return quotes[randonNumber];
 }
 
 /***
  * `printQuote` function
-***/
-function printQuote(){
-  
-  let randomQuoteObject = getRandomQuote(); 
-  let pQuote =`<p class="quote">${randomQuoteObject.quote}</p>
-              <p class="source">${randomQuoteObject.source}`
+ ***/
+function printQuote() {
+	let randomQuoteObject = getRandomQuote();
+	let pQuote = `<p class="quote">${randomQuoteObject.quote}</p>
+              <p class="source">${randomQuoteObject.source}`;
 
-  if (randomQuoteObject.citation) {
-    pQuote += `<span class="citation">${randomQuoteObject.citation}</span>`
-  }     
-    
- if(randomQuoteObject.year){
-    pQuote += `<span class="year">${randomQuoteObject.year}</span>`;
-   }                
-  pQuote += `</p>`
-                 
-               
-  return document.getElementById('quote-box').innerHTML= pQuote;
+	if (randomQuoteObject.citation) {
+		pQuote += `<span class="citation">${randomQuoteObject.citation}</span>`;
+	}
+
+	if (randomQuoteObject.year) {
+		pQuote += `<span class="year">${randomQuoteObject.year}</span>`;
+	}
+	if (randomQuoteObject.profession) {
+		pQuote += `<span class="year">${randomQuoteObject.profession}</span>`;
+	}
+	pQuote += `</p>`;
+
+	return (document.getElementById("quote-box").innerHTML = pQuote);
 }
 printQuote();
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
+ ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, true);
-
-
+document
+	.getElementById("load-quote")
+	.addEventListener("click", printQuote, true);
